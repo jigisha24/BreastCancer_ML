21b9ee7551309deb25004ab5b0b0b019cb71dfb3
jigisha24 <jigisha2436@gmail.com>
2026-04-17 11:39:01 +0530

    added model

---
+++ model_training.py
@@ -0,0 +1,39 @@
+
+
+from sklearn.datasets import load_breast_cancer
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
+import joblib
+import numpy as np
+
+
+data = load_breast_cancer()
+X = data.data          
+y = data.target        
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+scaler = StandardScaler()
+X_train_scaled = scaler.fit_transform(X_train)
+X_test_scaled = scaler.transform(X_test)
+
+
+model = LogisticRegression(max_iter=10000)
+model.fit(X_train_scaled, y_train)
+
+
+y_pred = model.predict(X_test_scaled)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+
+# ✅ Save in backend folder
+joblib.dump(model, "backend/model.pkl")
+joblib.dump(scaler, "backend/scaler.pkl")
+
+#Save feature means (used to fill missing 23 features)
+feature_means = X.mean(axis=0)       # shape (30,)
+joblib.dump(feature_means, "backend/feature_means.pkl")
+
+print("Saved: backend/model.pkl, backend/scaler.pkl, backend/feature_means.pkl")
